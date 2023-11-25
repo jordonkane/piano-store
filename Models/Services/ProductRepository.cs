692e1507a9a568c562c7e357fbdfef15fdf53363
jordonkane <kanejord99@outlook.com>
2023-11-24 19:42:41 -0500

    Update ProductRepository.cs

--- Models/Services/ProductRepository.cs
+++ Models/Services/ProductRepository.cs
@@ -4,19 +4,29 @@ namespace piano_store.Models.Services
 {
     public class ProductRepository : IProductRepository
     {
+        private List<Product> ProductsList = new List<Product>()
+        {
+            new Product{Id = 1, Name = "Piano 1", Price = 25, Detail = "Description", ImageUrl=""},
+            new Product{Id = 2, Name = "Piano 2", Price = 30, Detail = "Description", ImageUrl=""},
+            new Product{Id = 3, Name = "Piano 3", Price = 40, Detail = "Description", ImageUrl=""},
+        };
+
+        // Returns a list of all products
         public IEnumerable<Product> GetAllProducts()
         {
-            throw new NotImplementedException();
+            return ProductsList;
         }
 
+        // Returns details of products
         public Product GetProductDetail(int id)
         {
-            throw new NotImplementedException();
+            return ProductsList.FirstOrDefault(p => p.Id == id);
         }
 
+        // Returns all trending products
         public IEnumerable<Product> GetTrendingProducts()
         {
-            throw new NotImplementedException();
+            return ProductsList.Where(p => p.IsTrendingProduct);
         }
     }
 }
