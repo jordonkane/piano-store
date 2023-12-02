90ecf3ee000fdc057a9d4bdfd58c6837e81d8e3d
jordonkane <kanejord99@outlook.com>
2023-12-01 22:34:17 -0500

    Implemented additional entity framework migrations.

--- Data/PianoStoreDbContext.cs
+++ Data/PianoStoreDbContext.cs
@@ -12,5 +12,23 @@ namespace piano_store.Data
 
 		// Properties (will be mapped database table name at runtime)
 		public DbSet<Product> Products { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			// Get a new instance of the Product class and set its properties
+			modelBuilder.Entity<Product>().HasData(
+
+				new Product { Id = 1, Name = "Piano 1", Detail = "Some details", Price = 15, IsTrendingProduct = true, ImageUrl = "" },
+				new Product { Id = 2, Name = "Piano 2", Detail = "Some details", Price = 15, IsTrendingProduct = true, ImageUrl = "" },
+				new Product { Id = 3, Name = "Piano 3", Detail = "Some details", Price = 15, IsTrendingProduct = true, ImageUrl = "" },
+				new Product { Id = 4, Name = "Piano 4", Detail = "Some details", Price = 15, IsTrendingProduct = false, ImageUrl = "" },
+				new Product { Id = 5, Name = "Piano 5", Detail = "Some details", Price = 15, IsTrendingProduct = false, ImageUrl = "" },
+				new Product { Id = 6, Name = "Piano 6", Detail = "Some details", Price = 15, IsTrendingProduct = false, ImageUrl = "" },
+				new Product { Id = 7, Name = "Piano 7", Detail = "Some details", Price = 15, IsTrendingProduct = false, ImageUrl = "" }
+
+				);
+		}
 	}
 }
--- Migrations/20231202032805_SeedProductsTable.Designer.cs
+++ Migrations/20231202032805_SeedProductsTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using piano_store.Data;
 
@@ -10,9 +11,11 @@ using piano_store.Data;
 namespace pianostore.Migrations
 {
     [DbContext(typeof(PianoStoreDbContext))]
-    partial class PianoStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231202032805_SeedProductsTable")]
+    partial class SeedProductsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
