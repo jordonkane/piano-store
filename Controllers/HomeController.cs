2ca43eab4dbf3f1f67b5ff730de04a023f476ddb
jordonkane <kanejord99@outlook.com>
2023-12-02 00:23:51 -0500

    Integrate Bootstrap template.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using piano_store.Models.Interfaces;
+
+namespace piano_store.Controllers
+{
+	public class HomeController : Controller
+	{
+		private IProductRepository productRepository;
+
+		// Inject dependencies
+		public HomeController(IProductRepository productRepository)
+		{
+			this.productRepository = productRepository;
+		}
+
+		// Return view
+		public IActionResult Index()
+		{
+			// Get trending products
+			return View(productRepository.GetTrendingProducts());
+		}
+	}
+}
